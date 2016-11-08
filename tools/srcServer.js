b10f20087348ec91b2115e5d5364e5ce44e599f7
Restry Louis <michael.wei@foxmail.com>
2016-11-08 20:48:38 +0800

    go to env port

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -6,7 +6,8 @@ import open from 'open';
 
 /* eslint-disable no-console */
 
-const port = 8000;
+app.set('port', (process.env.PORT || 5000));
+
 const app = express();
 const compiler = webpack(config);
 
@@ -21,7 +22,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(app.get('port'), function(err) {
   if (err) {
     console.log(err);
   } else {
