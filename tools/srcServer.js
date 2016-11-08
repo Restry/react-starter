704d1dddd73a1f9e3db26ca946e53eb26313929b
Restry Louis <michael.wei@foxmail.com>
2016-11-08 20:51:49 +0800

    wrong fix

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -6,10 +6,11 @@ import open from 'open';
 
 /* eslint-disable no-console */
 
-app.set('port', (process.env.PORT || 5000));
 
 const app = express();
 const compiler = webpack(config);
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
